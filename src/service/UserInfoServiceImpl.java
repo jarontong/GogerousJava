403d2c741f9208e75d83993f0edc936df7923ff8
Administrator <1021661582@qq.com>
2018-04-18 15:24:49 +0800

    增加cookie刷新时效，增加获取验证码接口

--- src/service/UserInfoServiceImpl.java
+++ src/service/UserInfoServiceImpl.java
@@ -196,6 +196,7 @@ public class UserInfoServiceImpl implements IUserInfoService {
 
     @Override
     public JsonResponseDto getCode(String account) {
+        //TODO
         return null;
     }
 }
